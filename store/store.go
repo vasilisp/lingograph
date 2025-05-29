513ce5605c14437975ad3e8cfc6e08bfbb51a6c0
Vasilis Papavasileiou <vasilisp@users.noreply.github.com>
2025-05-29 12:14:47 +0000

    Add store documentation comments

--- store/store.go
+++ store/store.go
@@ -9,22 +9,28 @@ import (
 // stores, so types have to be the same globally
 var nextID int64
 
+// Store is a heterogeneous key-value map.
 type Store struct {
 	vars *sync.Map
 }
 
+// NewStore creates a new Store.
 func NewStore() Store {
 	return Store{vars: &sync.Map{}}
 }
 
+// Var is a unique identifier for a variable in the Store.
 type Var[T any] struct {
 	id int64
 }
 
+// FreshVar creates a new Var with a unique ID.
 func FreshVar[T any]() Var[T] {
 	return Var[T]{id: atomic.AddInt64(&nextID, 1)}
 }
 
+// Get retrieves the value of a Var from the Store. The second return value
+// indicates whether the variable was found.
 func Get[T any](r Store, v Var[T]) (T, bool) {
 	var valT T
 
@@ -41,18 +47,23 @@ func Get[T any](r Store, v Var[T]) (T, bool) {
 	return valT, true
 }
 
+// Set sets the value of a Var in the Store.
 func Set[T any](r Store, v Var[T], val T) {
 	r.vars.Store(v.id, val)
 }
 
+// StoreRO is a read-only view of a Store.
 type StoreRO struct {
 	store Store
 }
 
+// RO returns a read-only view of the Store.
 func (r Store) RO() StoreRO {
 	return StoreRO{store: r}
 }
 
+// GetRO retrieves the value of a Var from the read-only Store. The second
+// return value indicates whether the variable was bound.
 func GetRO[T any](r StoreRO, v Var[T]) (T, bool) {
 	return Get(r.store, v)
 }
